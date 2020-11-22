d64766e7ff124939115671da721f2af959e59ee7
Jon Jessee <tjessee7624@gmail.com>
2020-11-22 15:08:41 -0700

    Initial commit. Copied over existing file structure.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+const PORT = 3000;
+const app = express();
+
+//Log network requests
+app.use(logger("dev"));
+
+//Data parsing middleware
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Staticly serve public directory
+app.use(express.static("public"));
+
+//Connect to mongoose
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//Routes
+app.use(require("./routes/api.js"));
+
+//Server listening for activity
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
